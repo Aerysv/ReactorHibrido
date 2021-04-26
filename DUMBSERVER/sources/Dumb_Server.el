27fa397692ab103c793af21cb11e80a9d5ba55fa
Aerysv <dmontesl1203@gmail.com>
2021-04-26 11:36:59 +0200

    Añadida implementación de influxdb

---
+++ DUMBSERVER/sources/Dumb_Server.el
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------------------
+ LIBRARY: DUMBSERVER
+ FILE: Dumb_Server
+ CREATION DATE: 26/04/2021
+-----------------------------------------------------------------------------------------*/
+COMPONENT Dumb_Server
+
+DECLS
+BOOLEAN Lectura_FlagMPC
+BOOLEAN Lectura_FlagRTO
+BOOLEAN Lectura_FlagMA
+DISCR REAL Lectura_q
+DISCR REAL Lectura_Fr
+DISCR REAL Lectura_T0
+DISCR REAL Lectura_Tc0
+DISCR REAL Lectura_T
+DISCR REAL Lectura_Tc
+DISCR REAL Lectura_Ca
+DISCR REAL Lectura_Cb
+DISCR REAL Lectura_Cc
+DISCR REAL Lectura_Cd
+DISCR REAL Lectura_Qrxn
+DISCR REAL Lectura_Bomba
+DISCR REAL Lectura_Valvula
+DISCR REAL Lectura_In_Volts00
+DISCR REAL Lectura_In_Volts01
+DISCR REAL Lectura_In_Volts02
+DISCR REAL Lectura_In_Volts03
+DISCR REAL Lectura_In_Volts04
+DISCR REAL Lectura_In_Volts06
+DISCR REAL Lectura_Out_Volts00
+DISCR REAL Lectura_Out_Volts01
+DISCR REAL Lectura_Out_Volts02
+DISCR REAL Lectura_pA
+DISCR REAL Lectura_pB
+DISCR REAL Lectura_pFr
+DISCR REAL Lectura_beta1
+DISCR REAL Lectura_beta2
+DISCR REAL Lectura_SP12
+DISCR REAL Lectura_PV12
+DISCR REAL Lectura_OP12
+DISCR REAL Lectura_SP22
+DISCR REAL Lectura_PV22
+DISCR REAL Lectura_OP22
+DISCR REAL Lectura_Cb_sp
+DISCR REAL Lectura_T_sp
+DISCR REAL Lectura_gamma1
+DISCR REAL Lectura_gamma2
+DISCR REAL Lectura_mu_J
+DISCR REAL Lectura_mu_g1
+DISCR REAL Lectura_mu_g2
+END COMPONENT
